3e6359f2476007d8b9a5781bbbe660fd28f9e9bf
chadlrnsn <chadlrnsn@gmail.com>
2026-03-16 23:40:43 +0500

    fix: set base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/markdown-to-bbcode/',
   plugins: [
     vue(),
     vueJsx(),
